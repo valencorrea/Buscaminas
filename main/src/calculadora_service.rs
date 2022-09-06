c45381dae3f9f79458e8f0b8e417bf85bbd1aa0e
valen <vcorrea@fi.uba.ar>
2022-09-06 00:22:00 -0300

    add tests unitarias del calcu service

--- main/src/calculadora_service.rs
+++ main/src/calculadora_service.rs
@@ -1,15 +1,17 @@
-pub fn cant_filas(bytes: &[u8]) -> usize {
+const ENTER_U8 : u8 = 10;
+
+pub fn cant_filas(bytes: &[u8]) -> usize { // PRE: TIENE ENTERS AL FINAL DE CADA FILA
     let mut cant_filas = 0;
 
     for caracter in bytes {
-        if *caracter == 10 {
+        if *caracter == ENTER_U8 {
             cant_filas += 1;
         }
     }
     cant_filas
 }
 
-pub fn cant_columnas(cant_bytes_matriz: usize, filas: usize) -> usize {
+pub fn cant_columnas(cant_bytes_matriz: usize, filas: usize) -> usize { // PRE: TIENE ENTERS AL FINAL DE CADA FILA
     (cant_bytes_matriz / filas) - 1
 }
 
@@ -43,4 +45,20 @@ pub fn calcular_col_limite_der(indice_columnas: usize, cant_columnas: usize) -> 
     } else {
         indice_columnas + 2
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn matriz_nxm_calculo_cant_filas_retorna_n() {
+        assert_eq!(cant_filas(&[42, 42, 42, ENTER_U8, 42, 42, 42, ENTER_U8]), 2);
+    }
+
+    #[test]
+    fn matriz_nxm_calculo_cant_columnas_retorna_m() {
+        assert_eq!(cant_columnas(8, 2), 3);
+    }
+
 }
