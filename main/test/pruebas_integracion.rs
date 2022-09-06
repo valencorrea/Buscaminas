732ff835253b844c7043f2812312a6f175688df6
valen <vcorrea@fi.uba.ar>
2022-09-06 01:40:25 -0300

    add test de integracion (no la corre)

---
+++ main/test/pruebas_integracion.rs
@@ -0,0 +1,8 @@
+use crate::buscaminas_service;
+
+    #[test] // <- no la corre
+    fn descubrir_minas_input_3x3_1_mina() {
+        let string_input = String::from("...\n...\n..*\n");
+        let string_output = String::from("...\n.11\n.1*\n");
+        assert_eq!(buscaminas_service::descubrir_minas(string_input), string_output);
+    }
